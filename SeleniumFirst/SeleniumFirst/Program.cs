5c696907dde4488c92dde2da1ff8ef1484fcc614
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-07-29 10:25:21 -0400

    Demo of basic test

--- SeleniumFirst/SeleniumFirst/Program.cs
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -5,25 +5,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace SeleniumFirst
 {
     class Program
     {
+        // Create reference for our browser
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
-            // Create reference for our browser
-            IWebDriver driver = new ChromeDriver();
+        }
 
+        [SetUp]
+        public void Initialize()
+        {
             // Navigate to Google page
             driver.Navigate().GoToUrl("https://www.google.com");
+            Console.WriteLine("Opened URL");
+        }
 
+        [Test]
+        public void ExecuteTest()
+        {
             // Find the element (search box)
             IWebElement element = driver.FindElement(By.Name("q"));
 
             // Perform operations
             element.SendKeys("Test the input");
+
+            Console.WriteLine("Executed Test");
+        }
+
+        [Test]
+        public void NextTest()
+        {
+            Console.WriteLine("Next Test");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
             driver.Close();
+            Console.WriteLine("Closed the browser");
         }
     }
 }
